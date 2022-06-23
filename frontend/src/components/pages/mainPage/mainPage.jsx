cbe3db485e29588ea00d1599661430ff401ebbc7
sansay500 <borodachev@gmail.com>
2022-06-23 10:29:01 +0300

    fetch

--- frontend/src/components/pages/mainPage/mainPage.jsx
+++ frontend/src/components/pages/mainPage/mainPage.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
 import style from './mainPage.module.css';
 import { RegisterForm } from './registerForm/registerForm';
 
 export const MainPage = (props) => {
+	const [users, setUsers] = useState('pups');
+	useEffect( () => {
+ 		     fetch('http://localhost:8000/api/user')
+			.then((response) => response.json())
+			.then((actualData)=>console.log(actualData)
+			)},[]);
+
 	return (
 		<main className={`container ${style.container}`}>
+
 			<div className={`${style.blue}`}>
-				1
 				<RegisterForm />
 			</div>
-
 			<div className={`${style.green}`}>2</div>
-
 			<div className={`${style.red}`}>3</div>
 		</main>
 	);
