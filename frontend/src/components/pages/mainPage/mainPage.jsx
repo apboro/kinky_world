100fdf51d807aefe9c20e0e02b9feebe52b92cd7
sansay500 <borodachev@gmail.com>
2022-06-24 11:36:04 +0300

    Fetch

--- frontend/src/components/pages/mainPage/mainPage.jsx
+++ frontend/src/components/pages/mainPage/mainPage.jsx
@@ -5,18 +5,24 @@ import { RegisterForm } from './registerForm/registerForm';
 export const MainPage = (props) => {
 	const [users, setUsers] = useState('pups');
 	useEffect( () => {
- 		     fetch('http://localhost:8000/api/user')
+ 		     fetch('http://kinkyapi.alevi.space/api/user')
 			.then((response) => response.json())
-			.then((actualData)=>console.log(actualData)
+			.then((actualData)=>setUsers(actualData)
 			)},[]);
-
+	function handleClick(){
+		console.log(users)
+	}
 	return (
 		<main className={`container ${style.container}`}>
 
+			{/*{users.map((u)=>(<li>kuk</li>))}*/}
 			<div className={`${style.blue}`}>
 				<RegisterForm />
 			</div>
-			<div className={`${style.green}`}>2</div>
+			<div className={`${style.green}`}>2
+				<button value="submit" onClick={handleClick}>Click Me</button>
+			</div>
+
 			<div className={`${style.red}`}>3</div>
 		</main>
 	);
